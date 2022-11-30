8fb1e8802b11039e734a103df506cc8c689f93ac
Choiseokjae <829520k@gmail.com>
2022-11-30 17:08:12 +0900

    commit

---
+++ TS/index.js
@@ -0,0 +1,6 @@
+var test = '컴파일';
+var 이름 = { name: 'choi' };
+function f(x) {
+    return x * 2;
+}
+f(123123);
---
+++ TS/index.ts
@@ -0,0 +1,12 @@
+let test :string = '컴파일';
+
+let 이름 :{ name? :string} = { name: 'choi'};
+
+type Mine  = string | number ;
+
+function f(x :number) :number {
+    return x *2
+}
+
+
+f(123123);
